dc62575627ab796775ad43cb524cd529c49a0e7b
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-12 16:45:04 +0900

    :zap: feature: 크롤링 DTO 설계

--- src/main/java/com/joo/digimon/crawling/dto/CrawlingCardDto.java
+++ src/main/java/com/joo/digimon/crawling/dto/CrawlingCardDto.java
@@ -1,7 +1,42 @@
 package com.joo.digimon.crawling.dto;
 
+import com.joo.digimon.crawling.enums.CardType;
+import com.joo.digimon.crawling.enums.Rarity;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class CrawlingCardDto {
+
+    @NonNull
+    String cardNo;
+    @NonNull
+    Rarity rarity;
+    @NonNull
+    CardType cardType;
+
+    String lv;
+
+    @NonNull
+    Boolean isParallel;
+    @NonNull
+    String cardName;
+    String form;
+    String attribute;
+    String type;
+    Integer dP;
+    Integer playCost;
+    String digivolveCost1;
+    String digivolveCost2;
+    String effect;
+    String sourceEffect;
+    String note;
+
+    @NonNull
+    String imgUrl;
+
+
+
+
+
 }
---
+++ src/main/java/com/joo/digimon/crawling/enums/CardType.java
@@ -0,0 +1,5 @@
+package com.joo.digimon.crawling.enums;
+
+public enum CardType {
+    DIGIMON, TAMER, OPTION, DIGITAMA
+}
