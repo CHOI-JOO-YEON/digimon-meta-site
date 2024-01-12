f408efb025dda90a3bdff7c1e497c906ded8638f
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-12 22:41:35 +0900

    :zap: feature: Color Enum 추가

---
+++ src/main/java/com/joo/digimon/crawling/enums/Color.java
@@ -0,0 +1,14 @@
+package com.joo.digimon.crawling.enums;
+
+public enum Color {
+    RED, BLUE, YELLOW, GREEN, BLACK, PURPLE, WHITE;
+
+    public static Color getColorByString(String colorString) {
+        for (Color value : Color.values()) {
+            if(colorString.toUpperCase().equals(value.name())){
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("잘못된 색깔 문자열");
+    }
+}
