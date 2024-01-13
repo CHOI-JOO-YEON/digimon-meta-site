78cd2dbf6473cbe6fc9fdc7da6c4da48f28dd92c
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-13 17:53:57 +0900

    :zap: feature: DB에 반영되지 않은 크롤링 엔티티 select 기능

--- src/main/java/com/joo/digimon/crawling/repository/CrawlingCardRepository.java
+++ src/main/java/com/joo/digimon/crawling/repository/CrawlingCardRepository.java
@@ -4,6 +4,9 @@ import com.joo.digimon.crawling.model.CrawlingCardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CrawlingCardRepository extends JpaRepository<CrawlingCardEntity,Integer> {
+    List<CrawlingCardEntity> findByCardImgEntityIsNullAndParallelCardImgEntityIsNull();
 }
