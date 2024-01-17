6422f155385d02eed0d0dfc0f18fcd0ed90105ea
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-17 21:31:09 +0900

    :zap: feature: CardImg에 엔티티 그래프 적용

--- src/main/java/com/joo/digimon/card/repository/CardImgRepository.java
+++ src/main/java/com/joo/digimon/card/repository/CardImgRepository.java
@@ -1,9 +1,13 @@
 package com.joo.digimon.card.repository;
 
 import com.joo.digimon.card.model.CardImgEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
 public interface CardImgRepository extends JpaRepository<CardImgEntity,Integer> {
+    @EntityGraph("CardImgEntity.detail")
+    List<CardImgEntity> findByUploadUrlIsNull();
 }
