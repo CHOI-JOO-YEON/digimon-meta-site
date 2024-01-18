7f5e9b6655d94dc3707446bcdf8d12ab2f9d0085
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-18 11:26:31 +0900

    :zap: feature: CardEntity로 검색 추가

--- src/main/java/com/joo/digimon/card/repository/CardImgRepository.java
+++ src/main/java/com/joo/digimon/card/repository/CardImgRepository.java
@@ -1,13 +1,19 @@
 package com.joo.digimon.card.repository;
 
+import com.joo.digimon.card.model.CardEntity;
 import com.joo.digimon.card.model.CardImgEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CardImgRepository extends JpaRepository<CardImgEntity,Integer> {
     @EntityGraph("CardImgEntity.detail")
     List<CardImgEntity> findByUploadUrlIsNull();
+
+    @EntityGraph("CardImgEntity.detail")
+    Optional<CardImgEntity> findByCardEntity(CardEntity cardEntity);
 }
