f8c159be7ed468f98fa0fcec1f0bb041b6f09e51
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-23 21:18:00 +0900

    :zap: feature: 정렬 Valid 어노테이션 추가

---
+++ src/main/java/com/joo/digimon/annotation/valid/OrderValidator.java
@@ -0,0 +1,28 @@
+package com.joo.digimon.annotation.valid;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OrderValidator implements ConstraintValidator<OrderValid, String> {
+
+    private Set<String> options;
+    @Override
+    public void initialize(OrderValid constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+        options = new HashSet<>();
+        options.add("cardNo");
+        options.add("cardName");
+        options.add("dp");
+        options.add("playCost");
+        options.add("lv");
+
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return options.contains(value);
+    }
+}
