e9703567c39ed7981406628fe87f11753f48c816
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-01-23 21:17:40 +0900

    :zap: feature: 문자열 Enum 컨버터 추가

--- src/main/java/com/joo/digimon/config/WebConfig.java
+++ src/main/java/com/joo/digimon/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.joo.digimon.config;
 
+import com.joo.digimon.util.StringToEnumConverterFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +16,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(new StringToEnumConverterFactory());
     }
 }
