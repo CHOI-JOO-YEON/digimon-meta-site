d7804424583114fc83e80eccc1c53d6e9fa397ef
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-03-11 21:14:44 +0900

    :zap: feature: 닉네임 서비스 수정

--- src/main/java/com/joo/digimon/user/service/NicknameServiceImpl.java
+++ src/main/java/com/joo/digimon/user/service/NicknameServiceImpl.java
@@ -1,11 +1,27 @@
 package com.joo.digimon.user.service;
 
+import com.joo.digimon.card.model.CardEntity;
+import com.joo.digimon.card.repository.CardRepository;
+import com.joo.digimon.crawling.enums.CardType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class NicknameServiceImpl implements NicknameService{
+@RequiredArgsConstructor
+public class NicknameServiceImpl implements NicknameService {
+    private final CardRepository cardRepository;
+
+
     @Override
     public String generateNickname() {
-        return "사용자";
+        List<CardEntity> allEntities = cardRepository.findByCardType(CardType.DIGIMON);
+        if (allEntities.isEmpty()) {
+            return "이그드라실";
+        }
+        int randomIndex = (int) (Math.random() * allEntities.size());
+        return allEntities.get(randomIndex).getCardName();
     }
+
 }
