04eaf25e7d05419c840ab698e9eb0eef939aa7f9
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-03-14 12:08:57 +0900

    :zap: feature: 로그아웃 기능 추가

--- src/main/java/com/joo/digimon/user/controller/AccountController.java
+++ src/main/java/com/joo/digimon/user/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.joo.digimon.user.controller;
 
 import com.joo.digimon.user.dto.LoginResponseDto;
 import com.joo.digimon.user.dto.UsernameLoginRequestDto;
+import com.joo.digimon.user.service.NicknameService;
 import com.joo.digimon.user.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,27 +12,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
     private final UserService userService;
+    private final NicknameService nicknameService;
 
     @GetMapping("/token/kakao")
     public ResponseEntity<?> getKakaoToken(@RequestParam String code, HttpServletResponse response) throws IOException {
         LoginResponseDto loginResponseDto = userService.getKakaoToken(code);
         setTokenCookie(response, loginResponseDto);
-        return new ResponseEntity<>(loginResponseDto,HttpStatus.OK);
+        return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
     }
 
     @PostMapping("/login/username")
-    public ResponseEntity<?> loginUsername(@RequestBody UsernameLoginRequestDto usernameLoginRequestDto,HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> loginUsername(@RequestBody UsernameLoginRequestDto usernameLoginRequestDto, HttpServletResponse response) throws IOException {
         LoginResponseDto loginResponseDto = userService.usernameLogin(usernameLoginRequestDto);
         setTokenCookie(response, loginResponseDto);
-        return new ResponseEntity<>(loginResponseDto,HttpStatus.OK);
+        return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
     }
-
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout( HttpServletResponse response) {
+        invalidateTokenCookie(response);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     private static void setTokenCookie(HttpServletResponse response, LoginResponseDto loginResponseDto) {
         Cookie jwtCookie = new Cookie("JWT_TOKEN", loginResponseDto.getAccessToken());
@@ -41,5 +49,12 @@ public class AccountController {
         response.addCookie(jwtCookie);
     }
 
+    private static void invalidateTokenCookie(HttpServletResponse response) {
+        Cookie jwtCookie = new Cookie("JWT_TOKEN", "");
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setMaxAge(0);
+        jwtCookie.setPath("/");
+        response.addCookie(jwtCookie);
+    }
 
 }
