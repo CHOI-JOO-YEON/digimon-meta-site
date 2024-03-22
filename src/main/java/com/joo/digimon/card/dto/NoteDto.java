7da2eff5f6710e431249074ce6e1c1a7514af9c5
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-03-22 22:25:21 +0900

    :zap: feature: noteDto 반환 내용 추가

--- src/main/java/com/joo/digimon/card/dto/NoteDto.java
+++ src/main/java/com/joo/digimon/card/dto/NoteDto.java
@@ -1,16 +1,21 @@
 package com.joo.digimon.card.dto;
 
 import com.joo.digimon.card.model.NoteEntity;
+import com.joo.digimon.crawling.enums.CardOrigin;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 public class NoteDto {
     Integer noteId;
     String name;
-
+    LocalDate releaseDate;
+    CardOrigin cardOrigin;
     public NoteDto(NoteEntity noteEntity) {
         this.noteId=noteEntity.getId();
         this.name = noteEntity.getName();
-
+        this.releaseDate = noteEntity.getReleaseDate();
+        this.cardOrigin = noteEntity.getCardOrigin();
     }
 }
