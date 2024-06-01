08a2592f63bb439e98b45bb7962a8516ec3a3955
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-06-02 01:51:34 +0900

    :zap: feature: 닉네임 서비스 수정

--- src/main/java/com/joo/digimon/user/service/NicknameService.java
+++ src/main/java/com/joo/digimon/user/service/NicknameService.java
@@ -1,6 +1,13 @@
 package com.joo.digimon.user.service;
 
 
+import com.joo.digimon.card.model.CardEntity;
+import com.joo.digimon.global.enums.CardType;
+
+import java.util.List;
+
 public interface NicknameService {
     String generateNickname();
+
+    List<CardEntity> findByCardTypeAndIsOnlyEnCardIsNullOrIsFalse(CardType cardType);
 }
