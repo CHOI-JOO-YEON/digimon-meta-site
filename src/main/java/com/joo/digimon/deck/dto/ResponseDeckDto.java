dcf755724a925bfa3d5d943503acde645b26116f
CHOI-JOO-YEON <genboyjy@gmail.com>
2024-07-17 10:05:27 +0900

    :zap: feature: 카드 중복 코드 제거

--- src/main/java/com/joo/digimon/deck/dto/ResponseDeckDto.java
+++ src/main/java/com/joo/digimon/deck/dto/ResponseDeckDto.java
@@ -1,18 +1,14 @@
 package com.joo.digimon.deck.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.joo.digimon.card.model.CardCombineTypeEntity;
+import com.joo.digimon.card.dto.CardDto;
 import com.joo.digimon.card.model.CardImgEntity;
-import com.joo.digimon.global.enums.CardType;
 import com.joo.digimon.global.enums.Color;
-import com.joo.digimon.global.enums.Form;
-import com.joo.digimon.global.enums.Rarity;
 import com.joo.digimon.deck.model.DeckCardEntity;
 import com.joo.digimon.deck.model.DeckColor;
 import com.joo.digimon.deck.model.DeckEntity;
 import lombok.Data;
+import lombok.Getter;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -53,79 +49,20 @@ public class ResponseDeckDto {
         cards.add(new Card(cardImgEntity,cnt,prefixUrl));
     }
 
-    @Data
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private class Card {
+
+    @Getter
+    private class Card extends CardDto {
         Integer cnt;
-        Integer cardId;
-        String cardNo;
-        String cardName;
-        Integer lv;
-        Integer dp;
-        Integer playCost;
-        Integer digivolveCost1;
-        Integer digivolveCondition1;
-        Integer digivolveCost2;
-        Integer digivolveCondition2;
-        String effect;
-        String sourceEffect;
-        Color color1;
-        Color color2;
-        Rarity rarity;
-        CardType cardType;
-        Form form;
-        String attributes;
-        List<String> types;
-        String imgUrl;
-        String smallImgUrl;
-        Boolean isParallel;
-        String sortString;
-        String noteName;
-        Integer noteId;
-        LocalDate releaseDate;
-        Boolean isEn;
         public Card(CardImgEntity card,Integer cnt, String prefixUrl) {
+            super(card,prefixUrl);
             this.cnt=cnt;
-            createCard(card, prefixUrl);
         }
 
-        private void createCard(CardImgEntity card, String prefixUrl) {
-            this.cardId = card.getId();
-            this.cardNo = card.getCardEntity().getCardNo();
-            this.cardName = card.getCardEntity().getCardName();
-            this.lv = card.getCardEntity().getLv();
-            this.dp = card.getCardEntity().getDp();
-            this.playCost = card.getCardEntity().getPlayCost();
-            this.digivolveCost1 = card.getCardEntity().getDigivolveCost1();
-            this.digivolveCondition1 = card.getCardEntity().getDigivolveCondition1();
-            this.digivolveCost2 = card.getCardEntity().getDigivolveCost2();
-            this.digivolveCondition2 = card.getCardEntity().getDigivolveCondition2();
-            this.effect = card.getCardEntity().getEffect();
-            this.sourceEffect = card.getCardEntity().getSourceEffect();
-            this.color1 = card.getCardEntity().getColor1();
-            this.color2 = card.getCardEntity().getColor2();
-            this.rarity = card.getCardEntity().getRarity();
-            this.cardType = card.getCardEntity().getCardType();
-            this.form = card.getCardEntity().getForm();
-            this.attributes = card.getCardEntity().getAttribute();
-            this.types = new ArrayList<>();
-            for (CardCombineTypeEntity cardCombineTypeEntity : card.getCardEntity().getCardCombineTypeEntities()) {
-                types.add(cardCombineTypeEntity.getTypeEntity().getName());
-            }
-            this.imgUrl = prefixUrl + card.getUploadUrl();
-            this.isParallel = card.getIsParallel();
-            this.sortString= card.getCardEntity().getSortString();
-            this.smallImgUrl= prefixUrl + card.getSmallImgUrl();
-            this.releaseDate=card.getCardEntity().getReleaseDate();
-            this.isEn=Boolean.TRUE.equals( card.getIsEnCard());
-            this.noteId = card.getNoteEntity().getId();
-            this.noteName=card.getNoteEntity().getName();
-        }
+
 
         public Card(DeckCardEntity deckCardEntity, String prefixUrl) {
-            cnt=deckCardEntity.getCnt();
-            CardImgEntity card = deckCardEntity.getCardImgEntity();
-            createCard(card, prefixUrl);
+            super(deckCardEntity.getCardImgEntity(),prefixUrl);
+            this.cnt=deckCardEntity.getCnt();
         }
 
     }
