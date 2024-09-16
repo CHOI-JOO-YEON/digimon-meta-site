2191c9061ed2997832393070ed7a08d5cce08074
p_joo <p_joo@pubg.com>
2024-09-16 23:31:11 +0900

    :zap: feature: scailingclient url 환경변수 값 대문자 제거

--- src/main/java/com/joo/digimon/card/externel/ScalingClient.java
+++ src/main/java/com/joo/digimon/card/externel/ScalingClient.java
@@ -4,7 +4,7 @@ import com.joo.digimon.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(url = "${feign.client.config.scalingClient.url}",name = "scalingClient", configuration = FeignClientConfig.class)
+@FeignClient(url = "${feign.client.config.scaling-client.url}",name = "scalingClient", configuration = FeignClientConfig.class)
 public interface ScalingClient {
 
     @PostMapping("/api")
