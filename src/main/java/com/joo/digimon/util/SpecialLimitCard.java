54336a2ca44ff225a3198664160e01ce6c484553
CHOI-JOO-YEON <genboyjy@gmail.com>
2025-07-06 02:14:50 +0900

    매주 제한 예외 카드 추가

--- src/main/java/com/joo/digimon/util/SpecialLimitCard.java
+++ src/main/java/com/joo/digimon/util/SpecialLimitCard.java
@@ -15,6 +15,8 @@ public class SpecialLimitCard {
         cardMap.put("BT6-085", 50);
         cardMap.put("EX2-046", 50);
         cardMap.put("BT11-061", 50);
+        cardMap.put("EX9-048", 50);
+        cardMap.put("BT22-079", 50);
     }
     public static int getCardLimit(String cardNo){
         if (cardMap.containsKey(cardNo)) {
